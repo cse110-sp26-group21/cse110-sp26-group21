f9d72d3c87a0bfbf41eab4ce28183e9bd2f1d33f
kcharry <kathycharry27@gmail.com>
2026-05-27 17:13:44 -0700

    completed basic typing logic

--- scripts/typing.js
+++ scripts/typing.js
@@ -1,11 +1,55 @@
 // typing.js
 
-// handles typing input
+/**
+ *  handles typing input
+ * checks correct/incorrect characters
+ * tracks typing progress
+ * calculates accuracy
+ * detects completed snippets
+ */
 
-// checks correct/incorrect characters
+import {
+  asteroids
+} from './asteroids.js';
 
-// tracks typing progress
+const typingInput =
+  document.querySelector('#typing-input');
 
-// calculates accuracy
 
-// detects completed snippets
+/**
+ * Initializes typing listener
+ * @param {Function} onDestroy Callback function
+ * when asteroid is destroyed
+ */
+export function initializeTyping(onDestroy) {
+
+  typingInput.addEventListener(
+    'input',
+    () => handleTyping(onDestroy)
+  );
+
+}
+
+
+
+/**
+ * Checks typed input against snippets
+ * @param {Function} onDestroy Callback
+ */
+function handleTyping(onDestroy) {
+
+  const typedText = typingInput.value;
+
+  asteroids.forEach((asteroid) => {
+
+    if (typedText === asteroid.snippet) {
+
+      onDestroy(asteroid);
+
+      typingInput.value = '';
+
+    }
+
+  });
+
+}
